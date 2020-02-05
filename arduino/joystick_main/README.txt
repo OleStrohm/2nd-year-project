93bb33b8ecc3c4cdf3e771ae8f6454a1de04e31d
Hassaan <hassaanhuda@gmail.com>
2020-02-05 13:13:40 +0000

    Updated joystick code with byte packing

--- arduino/joystick_main/README.txt
+++ arduino/joystick_main/README.txt
@@ -4,10 +4,28 @@ So UD and LR are the displacement from the midpoints (i.e. the position of the j
 
 For UD: 
 
-UP --> positive, max = 508/509
-DOWN --> negative, max = -489/-490
+UP --> positive, max = 513
+DOWN --> negative, max = -497
 
 For LR:
 
-RIGHT --> positive, max = 505/506
-LEFT --> negative, max = -500/-501
+RIGHT --> positive, max = 513
+LEFT --> negative, max = -507
+
+Natural stick drift seems to be  |x|, |y| < 3 i.e. define a dead zone between 3 and -3
+tbh not seen anything greater than 2 but choose 3 to be on a safer side
+
+---------------Encode---------------
+
+a 3 byte array called payload is used here
+payload[0] = first 8 bits of UD
+payload[1] = first 6 bits of LR + last 2 bits of UD
+payload[2] = identifier 0xA + last 4 bits of LR
+
+---------------Decode---------------
+
+UD = payload[0] + ((payload[1] & 0x2) << 8)
+LR = (payload[1] & 0xFC) + ((payload[2] & 0xF) << 8)
+Identifier = ((payload[2] >> 4) & 0xA)) this should give you 0xA
+
+I guess we can implement some sort of error mechanism here if we really wanted
--- arduino/joystick_main/joystick_main.ino
+++ arduino/joystick_main/joystick_main.ino
@@ -1,3 +1,4 @@
+
 int IUD = A1;
 int ILR = A0;
 
@@ -7,22 +8,53 @@ int UD = 0;
 int LR = 0;
 int UDMID = 0;
 int LRMID = 0;
+byte payload[3];
 
 void setup() {
-  Serial.begin(9600);
-  UDMID = analogRead(IUD);
-  LRMID = analogRead(ILR); 
-
+  joystick_setup();
 }
 
 void loop() {
+  joystick_loop();
+  delay(500);
+}
+
+void joystick_setup(){
+  Serial.begin(9600);
+  UDMID = analogRead(IUD);
+  LRMID = analogRead(ILR); 
+}
+
+void joystick_loop(){
+  
   UDRAW = analogRead(IUD);
   LRRAW = analogRead(ILR);
-  UD = UDRAW - UDMID;
+  UD = UDRAW - UDMID; // 
   LR = LRRAW - LRMID;
+  LR = LR * 4;
+  byte UD1 = highByte(UD);
+  byte UD0 = lowByte(UD);
+  byte LR1 = highByte(LR);
+  byte LR0 = lowByte(LR);
+
+  payload[0] = UD0; // first 8 bits of UD
+  payload[1] = (UD1 & 0x2) + LR0; // first 6 bits of LR and last 2 bits of UD
+  payload[2] = 0xA0 + (0xF & LR1); // hex A (1010) to represent joystick data and last 4 bits of LR
+
+  // to decode :
+  // UD = payload[0] + ((payload[1] & 0x2) << 8)
+  // LR = (payload[1] & 0xFC) + ((payload[2] & 0xF) << 8)
+  // Identifier = ((payload[2] >> 4) & 0xA)) this should give you 0xA
+  
   Serial.print("UD: ");
   Serial.print(UD);
-  Serial.print("\tLR: ");
-  Serial.println(LR);
-  delay(100);
+  Serial.print(" LR: ");
+  Serial.println(LR/4);
+  Serial.println(" Bytes: ");
+  Serial.println(payload[2]);
+  Serial.println(payload[1]);
+  Serial.println(payload[0]);
+
+  Serial.write(payload, 3);
+  
 }
