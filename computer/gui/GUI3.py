19e561fbd229ad96b1322ca81688d4b32dae2c39
ole strohm <Strohm99@gmail.com>
2020-03-07 12:45:31 +0000

    changed lots of settings, my bad

--- computer/gui/GUI3.py
+++ computer/gui/GUI3.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import tkinter.messagebox
 from tkinter import ttk
+from word2cmd import CommandController
 
 from threading import Thread
 #from arduinoControl import ArduinoController
@@ -22,7 +23,7 @@ class GUI:
 
         for key in self.modes:
             self.commands.load_cmds(key, path + self.modes[key].file_name)
-        print(commands.modes)
+        print("Command modes: ", commands.modes)
 
         self.root = None
         self.width = None
@@ -659,56 +660,6 @@ def setting_config(filename):
             settings_current[l[0]] = tmp_l
 
     return settings_current
-
-
-
-
-class CommandController:
-
-    def __init__(self):
-        self.modes = {}
-
-    def load_cmds(self, name, filename):
-        cmd_dict = {}
-        file = open(filename, 'r', encoding = 'utf-8')
-        lines = file.readlines()
-        file.close()
-        for line in lines:
-            line = line.strip('\n')
-            line = line.split(' ')
-            cmd_dict[line[0]] = line[1]
-
-        self.modes[name] = cmd_dict
-
-    def find_cmd(self, name, trans):
-        # if not self.modes.get(name):
-        #     return "empty", trans
-
-        if trans == "":
-            return "empty", ""
-
-        line = trans.split()
-        cmd_dict = self.modes[name]
-        cmd = 'empty'
-        left_string = line
-        for i in range(len(line)):
-            if cmd_dict.get(line[i]):
-                if i<len(line) and (cmd_dict[line[i]] == 'multi'): # if it is a a cmd that requires multiple words the first word has been designated the value multi to check for a second
-                    if len(line) == i+1:
-                        unprocessed_string = " ".join(left_string[i:])
-                        return cmd, unprocessed_string
-                    elif cmd_dict.get(line[i]+line[i+1]):
-                        print('cmd word' + line[i] +' ' + line[i+1] + 'cmd: ' + cmd_dict[line[i]+line[i+1]])
-                        cmd = cmd_dict[line[i]+line[i+1]]
-                        unprocessed_string = " ".join(left_string[i + 2:])
-                        return cmd, unprocessed_string
-                else:
-                    print('key: ' + line[i] + '\nvalue: ' + cmd_dict[line[i]])
-                    cmd = cmd_dict[line[i]]
-                    unprocessed_string = " ".join(left_string[i + 1:])
-                    return cmd, unprocessed_string
-
-        return cmd, ""
 
 if __name__ == "__main__":
     #arduino = ArduinoController()
